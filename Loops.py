bf7d62ef4e4e745cee4e951ca37ae6e12733997e
samprithkls <samprithkls@gmail.com>
2022-12-07 21:32:35 +0530

    Additional Command 3

---
+++ Loops.py
@@ -0,0 +1,9 @@
+"""
+    Loops
+    https://www.hackerrank.com/challenges/python-loops/
+
+"""
+n=int(input())
+
+for i in range (n):
+    print(i**2)
---
+++ Sum_is_Even_Odd.py
@@ -0,0 +1,8 @@
+s=[10,30,65,78,42,91]
+sum=0
+for i in s:
+    sum=sum+i 
+if(sum%2==0):
+    print("Even")
+else:
+    print("Odd")
